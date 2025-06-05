36945c45acbaf283d13fd8ebc1f6bf590d077acb
RayhanKarim1342 <akari033@uottawa.ca>
2025-06-05 11:46:36 -0400

    added the footer

--- techcare-repair-app/src/App.js
+++ techcare-repair-app/src/App.js
@@ -1,17 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import Home from "./pages/Home";
 import Book from "./pages/Book";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/book" element={<Book />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/book" element={<Book />} />
+        </Routes>
+      </Router>
+      <footer
+        className="footer bg-dark text-white d-flex justify-content-center align-items-center"
+        style={{ height: "48px" }}
+      >
+        <Container className="text-center m-0 p-0">
+          Rayhan Karim & Samarventhan Surendran
+        </Container>
+      </footer>
+    </>
   );
 }
 
