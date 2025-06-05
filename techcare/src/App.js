3e920845aa2997f752564e5e16cfd6aea07353b0
RayhanKarim1342 <akari033@uottawa.ca>
2025-06-05 10:52:45 -0400

    Reapply "Updated the book page"
    This reverts commit 7fac7865653e6a718a5cf7099c12905991f8259e.

--- techcare/src/App.js
+++ techcare/src/App.js
@@ -1,16 +1,24 @@
-import Navigation from "./components/Navigation";
-import HomeImage from "./components/HomeImage";
-import Services from "./components/Services";
-import Footer from "./components/Footer";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <Navigation />
-      <HomeImage />
-      <Services />
-      <Footer />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- techcare/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <div className="mt-5 p-1 bg-dark text-white text-center">
-      <p>Rayhan Karim & Samarventhan Surendran</p>
-    </div>
-  );
-}
-
-export default Footer;
